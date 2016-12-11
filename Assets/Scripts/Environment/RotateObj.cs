d8d17a3667293eaca3176f0b5f8b12d524370320
GrahamParkJenkins <259907@AIV.EDMC.STU>
2016-12-10 17:52:59 -0800

    added textures/animations -GP

---
+++ Assets/Scripts/Environment/RotateObj.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class RotateObj : MonoBehaviour {
+
+    public Vector3 mRotation;
+    public float mSpeed;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update ()
+    {
+        transform.Rotate(mRotation);
+	}
+}
